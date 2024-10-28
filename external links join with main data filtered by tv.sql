e791d2652ec72fa5635f9ace293ca8994e787e31
Simi <102866859+St4rLighr@users.noreply.github.com>
2024-10-28 11:33:20 +0000

    Create external links join with main data filtered by tv.sql
    Left join of API tables filtered to TV

---
+++ external links join with main data filtered by tv.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `xxx.xxx.joined_links_data` AS
+SELECT
+a.id,
+a.idMal,
+a.title,
+a.format,
+a.episodes,
+a.duration,
+a.status,
+a.startDay,
+a.startMonth,
+a.startYear,
+a.meanScore,
+a.averageScore,
+a.popularity,
+a.favourites,
+a.genres,
+a.site,
+a.isDisabled,
+a.type,
+a.scoreDistribution_amount,
+a.scoreDistribution_score,
+a.statusDistribution_score,
+a.statusDistribution_status,
+a.rankings_allTime,
+a.rankings_rank,
+a.rankings_id,
+a.rankings_context,
+a.rankings_format,
+a.rankings_type,
+a.rankings_year,
+a.tags, 
+el.sites AS external_sites,
+el.types AS external_types 
+
+FROM `xxx.xxx.to_join_8` AS a
+LEFT JOIN `xxx.xxx.to_join_links_8` AS el
+ON a.id = el.id
+WHERE format = 'TV'
